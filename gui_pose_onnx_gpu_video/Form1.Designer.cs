cc8b8d2e940e36acca1b7b865c019ac9ec1c69d8
Nyi-Nyi-Myo <nyinyi.m@kkumail.com>
2025-10-31 23:48:38 +0700

    codes uploaded

---
+++ gui_pose_onnx_gpu_video/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace gui_pose_onnx_gpu_video
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.ActiveCaption;
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox1.Location = new Point(99, 36);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1730, 924);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(542, 987);
+            button2.Name = "button2";
+            button2.Size = new Size(200, 60);
+            button2.TabIndex = 2;
+            button2.Text = "Run Inference";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ButtonShadow;
+            button3.FlatStyle = FlatStyle.Popup;
+            button3.ForeColor = SystemColors.ControlLightLight;
+            button3.Location = new Point(1159, 987);
+            button3.Name = "button3";
+            button3.Size = new Size(200, 60);
+            button3.TabIndex = 3;
+            button3.Text = "Load Folder";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(1398, 987);
+            button4.Name = "button4";
+            button4.Size = new Size(240, 60);
+            button4.TabIndex = 4;
+            button4.Text = "Run Grid Inference";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.BackColor = SystemColors.ActiveCaption;
+            button5.Location = new Point(775, 987);
+            button5.Name = "button5";
+            button5.Size = new Size(160, 60);
+            button5.TabIndex = 5;
+            button5.Text = "Webcam";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ButtonShadow;
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.ForeColor = SystemColors.ControlLightLight;
+            button1.Location = new Point(303, 987);
+            button1.Name = "button1";
+            button1.Size = new Size(200, 60);
+            button1.TabIndex = 0;
+            button1.Text = "Load Image";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button6
+            // 
+            button6.BackColor = SystemColors.ActiveCaption;
+            button6.Location = new Point(965, 987);
+            button6.Name = "button6";
+            button6.Size = new Size(160, 60);
+            button6.TabIndex = 6;
+            button6.Text = "Video";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += button6_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1920, 1080);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(pictureBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Makers - KeypointRCNN Pose Estimation Onnx GPU";
+            Shown += Form1_Shown;
+            Resize += Form1_Resize;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private PictureBox pictureBox1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+    }
+}
